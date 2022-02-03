bda116ab89a7968dd68efa640bd4bc42664bd4d2
flor aquino <flor@mandarinacyd.com.ar>
2022-02-03 17:46:16 -0300

    grita-web

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - The Big Picture v5.0.4 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+//$("#bg-img").fadeOut(5000); 
+
+//$("body").prepend('<div id="bg-img"></div>');
+//Declaración de métodos encadenados
+$("#bg-img").delay(3000)
+       .fadeOut(2000);
